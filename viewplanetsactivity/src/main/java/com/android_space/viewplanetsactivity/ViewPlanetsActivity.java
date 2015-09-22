963a6509b89fc3990ac6c6aef844111d387a2e42
Kristina Hager <kristina.hager@gmail.com>
2015-09-22 17:03:00 -0500

    returning a result

--- viewplanetsactivity/src/main/java/com/android_space/viewplanetsactivity/ViewPlanetsActivity.java
+++ viewplanetsactivity/src/main/java/com/android_space/viewplanetsactivity/ViewPlanetsActivity.java
@@ -1,16 +1,71 @@
 package com.android_space.viewplanetsactivity;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class ViewPlanetsActivity extends ActionBarActivity {
+    public final static String SYSTEM_NAME = "com.android_space.viewplanetsactivity.SYSTEM_NAME";
+    public final static String PLANETS_ARRAY = "com.android_space.viewplanetsactivity.PLANETS_ARRAY";
+    public static final String TAG = "ViewPlanetsActivity:";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_planets);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //http://developer.android.com/training/basics/intents/filters.html
+        //provides some info on letting other apps call your activity
+        //this documentation includes a bit more than I'll include here
+        //however, it's a useful reference
+
+        //example of getting parameters/info from invoking activity
+        stringBuilder.append("Information from calling application:\n");
+        Intent intent = getIntent();
+        String systemName = intent.getStringExtra(this.SYSTEM_NAME);
+        stringBuilder.append("planetary system name: " + systemName + "\n");
+
+        Bundle planetsBundle = this.getIntent().getExtras();
+        String[] planetNames = planetsBundle.getStringArray(this.PLANETS_ARRAY);
+        stringBuilder.append("with planets: \n");
+        for (String planetName: planetNames) {
+            stringBuilder.append(planetName + "\n");
+        }
+
+        //TODO - hook up to planetarysystems lib
+
+        //example of using library hello
+//        stringBuilder.append("\nMessage from the library in use:\n");
+//        stringBuilder.append(HelloManager.getGreetings());
+
+        TextView helloTextView = (TextView) findViewById(R.id.hello_message);
+        helloTextView.setText(stringBuilder.toString());
+
+    }
+
+    public void sendActivityResult(View view) {
+        String systemName = "temp system name";
+        Log.i(TAG, "sendActivityResult: returning the result: " + systemName
+                + ", with result code: " + Activity.RESULT_OK);
+        Intent result = new Intent("com.android_space.RESULT_ACTION");
+        //http://developer.android.com/training/sharing/send.html
+        //TODO: is putExtra the best way for a library to return a result?
+        result.setAction(Intent.ACTION_SEND);
+        result.putExtra(Intent.EXTRA_TEXT, "You made a planetary system named: " + systemName);
+        result.setType("text/plain");
+        setResult(Activity.RESULT_OK, result);
+        finish();
     }
 
     @Override
